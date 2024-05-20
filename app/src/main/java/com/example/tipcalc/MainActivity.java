bdbd0a162d808b1204cc2035af5d8ca89a0b9775
Dang Hai Son <soncma2811@gmail.com>
2024-05-20 09:19:06 +0700

    upload file

---
+++ app/src/main/java/com/example/tipcalc/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.tipcalc;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    // define variables for the widgets
+    private EditText billAmountEditText;
+    private TextView percentTextView;
+    private Button   percentUpButton;
+    private Button   percentDownButton;
+    private TextView tipTextView;
+    private TextView totalTextView;
+
+    // define the SharedPreferences object
+    private SharedPreferences savedValues;
+
+    // define instance variables that should be saved
+    private String billAmountString = "";
+    private float tipPercent = .15f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        // get references to the widgets
+        billAmountEditText = (EditText) findViewById(R.id.billAmountEditText);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        percentUpButton = (Button) findViewById(R.id.percentUpButton);
+        percentDownButton = (Button) findViewById(R.id.percentDownButton);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+
+        // set the listeners
+        billAmountEditText.setOnEditorActionListener(this);
+        percentUpButton.setOnClickListener(this);
+        percentDownButton.setOnClickListener(this);
+        // get SharedPreferences object
+        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+    }
+    @Override
+    public void onPause() {
+        // save the instance variables
+        Editor editor = savedValues.edit();
+        editor.putString("billAmountString", billAmountString);
+        editor.putFloat("tipPercent", tipPercent);
+        editor.commit();
+
+        super.onPause();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // get the instance variables
+        billAmountString = savedValues.getString("billAmountString", "");
+        tipPercent = savedValues.getFloat("tipPercent", 0.15f);
+
+        // set the bill amount on its widget
+        billAmountEditText.setText(billAmountString);
+
+        // calculate and display
+        calculateAndDisplay();
+    }
+    public void calculateAndDisplay() {
+
+        // get the bill amount
+        billAmountString = billAmountEditText.getText().toString();
+        float billAmount;
+        if (billAmountString.equals("")) {
+            billAmount = 0;
+        }
+        else {
+            billAmount = Float.parseFloat(billAmountString);
+        }
+
+        // calculate tip and total
+        float tipAmount = billAmount * tipPercent;
+        float totalAmount = billAmount + tipAmount;
+
+        // display the other results with formatting
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        tipTextView.setText(currency.format(tipAmount));
+        totalTextView.setText(currency.format(totalAmount));
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        percentTextView.setText(percent.format(tipPercent));
+    }
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+            calculateAndDisplay();
+        }
+        return false;
+    }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
